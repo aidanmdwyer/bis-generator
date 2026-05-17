cd3831d6414befe51e0435308abdfbe7439ef291
adwyer02 <aidandyr@gmail.com>
2026-05-17 18:57:48 -0500

    v2.1 fixed counter day

--- php/incrementCount.php
+++ php/incrementCount.php
@@ -2,7 +2,7 @@
 require_once 'db.php';
 
 $conn->query("
-    INSERT INTO `bis_count` (`month`, `count`) VALUES (CURRENT_DATE, 0) 
+    INSERT INTO `bis_count` (`month`, `count`) VALUES (DATE_FORMAT(CURRENT_DATE, '%Y-%m-01'), 1) 
     ON DUPLICATE KEY UPDATE `count` = `count` + 1;
 ");
 
